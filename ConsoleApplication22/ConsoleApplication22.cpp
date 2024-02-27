973e4e04b231dc0cbbf60f96005c113b35052925
yffyfyfyuiiyf <ssashka117@mail.ru>
2024-02-27 11:14:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication22/ConsoleApplication22.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int x,y;
+	printf("Vvedite chislo x: ");
+	scanf_s("%d", &x);
+
+	
+	y = pow(x, 3) + pow(x, 2) - 10;
+
+	printf_s("Znachenie vyrazhenia ravno %d,\n", x, y);
+
+	return(0);
+}
